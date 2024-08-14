53b19a07e3f85f7b3f9c5e79748aa94d64ae70e6
Vishal sivanath <157196999+Vishalsivanath@users.noreply.github.com>
2024-08-14 15:13:25 +0530

    Add files via upload

---
+++ Crud.aspx
@@ -0,0 +1,91 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Crud.aspx.cs" Inherits="CrudOperation.Crud" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    
+    <title></title>
+    <style type="text/css">
+        .Eu_DataTable {}
+    </style>
+</head> 
+<body>
+    <form id="form1" runat="server" autocomplete="off">
+        <div>
+            <asp:Table runat="server">
+                <asp:TableRow>
+                    <asp:TableCell>Name:</asp:TableCell>
+                    <asp:TableCell>
+                        <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                    </asp:TableCell>
+                </asp:TableRow>
+
+                
+                <asp:TableRow>
+                    <asp:TableCell>Gender:</asp:TableCell>
+                    <asp:TableCell>
+                        <asp:RadioButton ID="RdMale" runat="server" GroupName="Gender" Text="Male" />
+                        <asp:RadioButton ID="RdFemale" runat="server" GroupName="Gender" Text="FeMale" />
+                        <asp:RadioButton ID="RdOthers" runat="server" GroupName="Gender" Text="Others" />
+                    </asp:TableCell>
+                </asp:TableRow>
+
+                <asp:TableRow>
+                    <asp:TableCell>Role:</asp:TableCell>
+                    <asp:TableCell>
+                        <asp:DropDownList ID="ddlROle" runat="server">
+                            <asp:ListItem>--select--</asp:ListItem>
+                            <asp:ListItem>Dev</asp:ListItem>
+                            <asp:ListItem>Testing</asp:ListItem>
+                            <asp:ListItem>Build</asp:ListItem>
+                            <asp:ListItem>Sales</asp:ListItem>
+                        </asp:DropDownList>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>Sumbit</asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Button ID="btnSumbit"  runat="server" Text="Sumbit" OnClick="btn_Sumbit_Click" />
+                    </asp:TableCell>
+                </asp:TableRow>
+            </asp:Table>
+        </div>
+
+        <div>
+            <asp:GridView ID="GridView1" runat ="server" AutoGenerateColumns="false" DataKeyNames="empid" OnRowEditing="Gridview1_RowEditing" OnRowCancelingEdit="Gridview1_RowCancelingEdit" OnRowDeleting="GridView1_RowDeleting" OnRowUpdating="GridView1_RowUpdating">
+                <Columns>
+                    <asp:TemplateField HeaderText="ID">
+                        <ItemTemplate>
+                            <asp:TextBox ID="textId" runat="server" Text='<%#Bind("empid")%>' ></asp:TextBox>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="EmpName">
+                        <ItemTemplate>
+                            <asp:TextBox ID="text2" runat="server" Text='<%#Bind("empName") %>'></asp:TextBox>
+                        </ItemTemplate>
+                   </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="Gender">
+                        <ItemTemplate>
+                            <asp:Textbox ID="text3" runat="server" Text='<%#Bind("Gender") %>'></asp:Textbox>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+                    <asp:TemplateField HeaderText="empRole">
+                        <ItemTemplate>
+                             <asp:TextBox ID="text4" runat="server" Text='<%#Bind("empRole") %>'></asp:TextBox>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    
+    
+
+                    <asp:CommandField ButtonType="Link" ShowEditButton="true" ShowDeleteButton="true"/>
+
+                </Columns>     
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Crud.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace CrudOperation
+{
+    public partial class Crud : System.Web.UI.Page
+    {
+        string conn = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GridView();
+            }
+        }
+        protected void btn_Sumbit_Click(object sender, EventArgs e)
+        {
+            string gender;
+            if(RdFemale.Checked)
+            {
+                gender = "Female";
+            }
+            else if(RdMale.Checked)
+            {
+                gender = "Male";
+            }
+            else
+            {
+                gender = "others";
+            }
+            try
+            {
+                SqlConnection sc = new SqlConnection(conn);
+                SqlCommand cmd = new SqlCommand("Emp_ins", sc);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@empName",txtName.Text);
+                cmd.Parameters.AddWithValue("@Gender", gender);
+                cmd.Parameters.AddWithValue("@empRole", ddlROle.SelectedValue);
+                sc.Open();
+                cmd.ExecuteNonQuery();
+                sc.Close();
+                GridView();
+
+            }
+            catch(Exception ex)
+            {
+                throw (ex);
+            }
+        }
+        protected void GridView()
+        {
+            SqlConnection sc = new SqlConnection(conn);
+            SqlCommand cmd = new SqlCommand("Emp_se", sc);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+
+            DataSet dt = new DataSet();
+            sda.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+        
+
+        protected void Gridview1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            GridView();
+        }
+
+        protected void Gridview1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = e.RowIndex - 1;
+            GridView();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+            string name = (row.FindControl("text2") as TextBox).Text;
+            string gender = (row.FindControl("text3") as TextBox).Text;
+            string role = (row.FindControl("text4") as TextBox).Text;
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            SqlConnection sc = new SqlConnection(conn);
+            SqlCommand cmd = new SqlCommand("empse", sc);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            sc.Open();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@empid", id);
+            cmd.Parameters.AddWithValue("@empName",name);
+            cmd.Parameters.AddWithValue("@Gender", gender);
+            cmd.Parameters.AddWithValue("@empRole",role);
+            cmd.ExecuteNonQuery();
+            sc.Close();
+            GridView1.EditIndex = -1;
+            GridView();
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            SqlConnection sc = new SqlConnection(conn);
+            SqlCommand cmd = new SqlCommand("deleteemp", sc);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@empid", id);
+            sc.Open();
+            cmd.ExecuteNonQuery();
+            sc.Close();
+            GridView();
+        }
+    }
+}
+
