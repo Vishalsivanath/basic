d15c231f22e541cc1d849ce300c5a125c3e8069a
Vishal sivanath <157196999+Vishalsivanath@users.noreply.github.com>
2024-08-28 16:00:46 +0530

    Update Crud.aspx.cs

--- Crud.aspx.cs
+++ Crud.aspx.cs
@@ -7,12 +7,11 @@ using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Configuration;
 using System.Data;
-
-namespace CrudOperation
+namespace Grid
 {
-    public partial class Crud : System.Web.UI.Page
+    public partial class Gridview : System.Web.UI.Page
     {
-        string conn = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+        string conn = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
@@ -58,7 +57,7 @@ namespace CrudOperation
             {
                 throw (ex);
             }
-            
+
         }
         protected void GridView()
         {
@@ -73,67 +72,6 @@ namespace CrudOperation
             GridView1.DataBind();
         }
 
-
-        protected void Gridview1_RowEditing(object sender, GridViewEditEventArgs e)
-        {
-            GridView1.EditIndex = e.NewEditIndex;
-            
-            GridView();
-        }
-
-        protected void Gridview1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
-        {
-            GridView1.EditIndex = e.RowIndex - 1;
-            GridView();
-        }
-
-        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
-        {
-            
-            GridViewRow row = GridView1.Rows[e.RowIndex];
-            string name = (row.FindControl("text2") as TextBox).Text;
-            string gender = (row.FindControl("text3") as TextBox).Text;
-            string role = (row.FindControl("text4") as TextBox).Text;
-            DropDownList ddl1 = (row.FindControl("ddlcountry") as DropDownList);
-            int country = ddl1.SelectedIndex+100;
-            DropDownList ddl2 = (row.FindControl("ddlstate") as DropDownList);
-            int state = ddl2.SelectedIndex+1000;
-            DropDownList ddl3 = (row.FindControl("ddlcity") as DropDownList);
-            int city = ddl3.SelectedIndex+100;
-
-            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
-            SqlConnection sc = new SqlConnection(conn);
-            SqlCommand cmd = new SqlCommand("UpdateEmploye", sc);
-            cmd.CommandType = System.Data.CommandType.StoredProcedure;
-            sc.Open();
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@empid", id);
-            cmd.Parameters.AddWithValue("@empName", name);
-            cmd.Parameters.AddWithValue("@Gender", gender);
-            cmd.Parameters.AddWithValue("@empRole", role);
-            cmd.Parameters.AddWithValue("@countryid", country);
-            cmd.Parameters.AddWithValue("@stateid", state);
-            cmd.Parameters.AddWithValue("@cityid", city);
-            cmd.ExecuteNonQuery();
-            sc.Close();
-            GridView1.EditIndex = -1;
-            GridView();
-        }
-
-
-        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
-        {
-            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
-            SqlConnection sc = new SqlConnection(conn);
-            SqlCommand cmd = new SqlCommand("employeDelete", sc);
-            cmd.CommandType = System.Data.CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@empid", id);
-            sc.Open();
-            cmd.ExecuteNonQuery();
-            sc.Close();
-            GridView();
-        }
-
         protected void BindCountries()
         {
             SqlConnection sc = new SqlConnection(conn);
@@ -146,12 +84,14 @@ namespace CrudOperation
             ddlCountry.DataValueField = "Countryid";
             ddlCountry.DataBind();
             ddlCountry.Items.Insert(0, new ListItem("Select Country", "0"));
+            GridView1.EditIndex = -1;
+            GridView();
         }
 
         protected void ddlCountry_SelectedIndexChanged(object sender, EventArgs e)
         {
             int id = Convert.ToInt32(ddlCountry.SelectedValue);
-            if(id>0)
+            if (id > 0)
             {
                 BindStates(id);
             }
@@ -176,9 +116,11 @@ namespace CrudOperation
             ddlState.DataValueField = "StateId";
             ddlState.DataBind();
             ddlState.Items.Insert(0, new ListItem("--Select State--", "0"));
+            GridView1.EditIndex = -1;
+            GridView();
         }
 
-     
+
 
         protected void ddlState_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -210,66 +152,192 @@ namespace CrudOperation
             ddlCity.Items.Insert(0, new ListItem("--Select City--", "0"));
         }
 
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            SqlConnection sc = new SqlConnection(conn);
+            SqlCommand cmd = new SqlCommand("employeDelete", sc);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@empid", id);
+            sc.Open();
+            cmd.ExecuteNonQuery();
+            sc.Close();
+            GridView();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+            string name = (row.FindControl("text2") as TextBox).Text;
+            string gender = (row.FindControl("text3") as TextBox).Text;
+            string role = (row.FindControl("text4") as TextBox).Text;
+            DropDownList ddl1 = (row.FindControl("ddlcountry") as DropDownList);
+            int country = Convert.ToInt32(ddl1.SelectedItem.Value);
+            DropDownList ddl2 = (row.FindControl("ddlstate") as DropDownList);
+            int state = Convert.ToInt32(ddl2.SelectedItem.Value);
+            DropDownList ddl3 = (row.FindControl("ddlcity") as DropDownList);
+            int city = Convert.ToInt32(ddl3.SelectedItem.Value);
+
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            SqlConnection sc = new SqlConnection(conn);
+            SqlCommand cmd = new SqlCommand("UpdateEmploye", sc);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            sc.Open();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@empid", id);
+            cmd.Parameters.AddWithValue("@empName", name);
+            cmd.Parameters.AddWithValue("@Gender", gender);
+            cmd.Parameters.AddWithValue("@empRole", role);
+            cmd.Parameters.AddWithValue("@countryid", country);
+            cmd.Parameters.AddWithValue("@stateid", state);
+            cmd.Parameters.AddWithValue("@cityid", city);
+            cmd.ExecuteNonQuery();
+            sc.Close();
+            GridView1.EditIndex = -1;
+            GridView();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            GridViewRow row = GridView1.Rows[e.NewEditIndex];
+            TextBox textId = (TextBox)row.FindControl("textId");
+            TextBox text2 = (TextBox)row.FindControl("text2");
+            TextBox text3 = (TextBox)row.FindControl("text3");
+            TextBox text4 = (TextBox)row.FindControl("text4");
+            DropDownList ddlcountry = (DropDownList)row.FindControl("ddlcountry");
+            DropDownList ddlstate = (DropDownList)row.FindControl("ddlstate");
+            DropDownList ddlcity = (DropDownList)row.FindControl("ddlcity");
+
+
+            Label lbltextId = (Label)row.FindControl("lbltextId");
+            Label lbltext2 = (Label)row.FindControl("lbltext2");
+            Label lbltext3 = (Label)row.FindControl("lbltext3");
+            Label lbltext4 = (Label)row.FindControl("lbltext4");
+            Label lbltext5 = (Label)row.FindControl("lbltext5");
+            Label lbltext6 = (Label)row.FindControl("lbltext6");
+            Label lbltext7 = (Label)row.FindControl("lbltext7");
+
+            textId.Visible = true;
+            text2.Visible = true;
+            text3.Visible = true;
+            text4.Visible = true;
+            ddlcountry.Visible = true;
+            ddlstate.Visible = true;
+            ddlcity.Visible = true;
+
+            lbltextId.Visible = false;
+            lbltext2.Visible = false;
+            lbltext3.Visible = false;
+            lbltext4.Visible = false;
+            lbltext5.Visible = false;
+            lbltext6.Visible = false;
+            lbltext7.Visible = false;
+            //GridView();
+
+            SqlConnection sc = new SqlConnection(conn);
+            SqlCommand cmd = new SqlCommand("GetAllCountries", sc);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            sc.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            ddlcountry.DataSource = dr;
+            ddlcountry.DataTextField = "Country";
+            ddlcountry.DataValueField = "Countryid";
+            ddlcountry.DataBind();
+            ddlcountry.Items.Insert(0, new ListItem("Select Country", "0"));
+
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = e.RowIndex - 1;
+            GridView();
+        }
+
         protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
         {
-            if (e.Row.RowType == DataControlRowType.DataRow && e.Row.RowState.HasFlag(DataControlRowState.Edit))
-
+            if (e.Row.RowType == DataControlRowType.DataRow)
             {
-
-
-
-                if (e.Row.RowType == DataControlRowType.DataRow)
-                {
-                    DropDownList ddl = (DropDownList)e.Row.FindControl("ddlcountry");
-                    if (ddl != null)
-                    {
-                        SqlConnection sc = new SqlConnection(conn);
-                        SqlCommand cmd = new SqlCommand("GetAllCountries", sc);
-                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                        sc.Open();
-                        SqlDataAdapter sda = new SqlDataAdapter(cmd);
-                        DataTable dt = new DataTable();
-                        sda.Fill(dt);
-                        ddl.DataSource = dt;
-                        ddl.DataTextField = "country";
-                        ddl.DataBind();
-                        ddl.Items.Insert(0, new ListItem("--Select Country--", "0"));
-                    }
-                    DropDownList ddl1 = (DropDownList)e.Row.FindControl("ddlstate");
-                    if (ddl1 != null)
-                    {
-                        SqlConnection sc = new SqlConnection(conn);
-                        SqlCommand cmd = new SqlCommand("States", sc);
-                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                        sc.Open();
-                        SqlDataAdapter sda = new SqlDataAdapter(cmd);
-                        DataTable dt = new DataTable();
-                        sda.Fill(dt);
-                        ddl1.DataSource = dt;
-                        ddl1.DataTextField = "state";
-
-                        ddl1.DataBind();
-                        ddl1.Items.Insert(0, new ListItem("--Select State--", "0"));
-                    }
-                    DropDownList ddl2 = (DropDownList)e.Row.FindControl("ddlcity");
-                    if (ddl2 != null)
-                    {
-                        SqlConnection sc = new SqlConnection(conn);
-                        SqlCommand cmd = new SqlCommand("Cities", sc);
-                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                        sc.Open();
-                        SqlDataAdapter sda = new SqlDataAdapter(cmd);
-                        DataTable dt = new DataTable();
-                        sda.Fill(dt);
-                        ddl2.DataSource = dt;
-                        ddl2.DataTextField = "city";
-
-                        ddl2.DataBind();
-                        ddl2.Items.Insert(0, new ListItem("--Select city--", "0"));
-                    }
-                }
+                
             }
+        }
+
+        protected void ddlcountry_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+            DropDownList ddlcountry = (DropDownList)sender;
+            GridViewRow gvrow = (GridViewRow)ddlcountry.NamingContainer;
+
+            DropDownList ddlgvcountry = (DropDownList)gvrow.FindControl("ddlcountry");
+            DropDownList ddlgvstate = (DropDownList)gvrow.FindControl("ddlstate");
+            DropDownList ddlgvcity = (DropDownList)gvrow.FindControl("ddlcity");
+
+            ddlgvstate.Items.Clear();
+            ddlgvcity.Items.Clear();
+
+            if (ddlgvcountry != null)
+            {
+                int selectedCountryValue = Convert.ToInt32(ddlgvcountry.SelectedValue);
+                
+                SqlConnection sc = new SqlConnection(conn);
+                SqlCommand cmd = new SqlCommand("GetAllStates", sc);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", selectedCountryValue);
+                sc.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                ddlgvstate.DataSource = dr;
+                ddlgvstate.DataTextField = "state";
+                ddlgvstate.DataValueField = "StateId";
+                ddlgvstate.DataBind();
+                ddlgvstate.Items.Insert(0, new ListItem("--Select State--", "0"));
+                ddlgvstate.Enabled = true;
+                ddlgvstate.Visible = true;
+
+            }
+            else
+            {
+                ddlgvstate.Enabled = false;
+                ddlgvstate.Items.Clear();
+                ddlgvstate.Items.Insert(0, new ListItem("--Select State--", "0"));
+                
+            }
+
+        }
+
+        protected void ddlstate_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+            DropDownList ddl = (DropDownList)sender;
+            GridViewRow gvrow = (GridViewRow)ddl.NamingContainer;
+            DropDownList ddlgvstate = (DropDownList)gvrow.FindControl("ddlstate");
+            DropDownList ddlgvcity = (DropDownList)gvrow.FindControl("ddlcity");
+            if (ddlgvstate != null)
+            {
+                int selectedCountryValue = Convert.ToInt32(ddlgvstate.SelectedValue);
+                SqlConnection sc = new SqlConnection(conn);
+                SqlCommand cmd = new SqlCommand("GetAllCities", sc);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Stateid", selectedCountryValue);
+                sc.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                ddlgvcity.DataSource = dr;
+                ddlgvcity.DataTextField = "city";
+                ddlgvcity.DataValueField = "Cityid";
+                ddlgvcity.DataBind();
+                ddlgvcity.Items.Insert(0, new ListItem("--Select City--", "0"));
+                ddlgvcity.Enabled = true;
+                ddlgvcity.Visible = true;
+            }
+            else
+            {
+                ddlgvcity.Enabled = false;
+                ddlgvstate.Items.Clear();
+                ddlgvstate.Items.Insert(0, new ListItem("--Select State--", "0"));
+
+            }
+        }
+
+        protected void btnadd_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("MainPage.aspx");
         }
     }
 }
-
